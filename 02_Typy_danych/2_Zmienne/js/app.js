79927dfad16da24eb1c20d1c6b339fe225e81797
wojtek0126 <wojtek0126@gmail.com>
2020-06-09 00:12:25 +0200

    Zadania JS

--- 02_Typy_danych/2_Zmienne/js/app.js
+++ 02_Typy_danych/2_Zmienne/js/app.js
@@ -1,17 +1,29 @@
 
 let number = 4;
-let string = "Napis";
-let mix = 2 + " Dwa";
-let boolean = true;
-let empty = null;
-
 console.log(number);
 console.log(typeof number);
+// Zmienna typu number o wartości 4.
+let string = "Napis";
 console.log(string);
 console.log(typeof string);
+// Zmienna typu string o wartości "napis".
+let mix = 2 + " Dwa";
 console.log(mix);
 console.log(typeof mix);
+// Zmienna typu string, zawiera konkatenację zmiennej typu number o wartości 2 i stringa o wartości "Dwa".
+let boolean = true;
 console.log(boolean);
 console.log(typeof boolean);
+// Zmienna typu boolean, zawiera wartość logiczną true.
+let empty = null;
 console.log(empty);
 console.log(typeof empty);
+//Zmienna typu object, zawiera wartość specjalną null.
+
+
+
+
+
+
+
+
