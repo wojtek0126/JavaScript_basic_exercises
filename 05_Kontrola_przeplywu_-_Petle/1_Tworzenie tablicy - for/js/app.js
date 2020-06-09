ac14e2f62c7172ad024de71c8ed7d2cbe218bf34
wojtek0126 <wojtek0126@gmail.com>
2020-06-09 17:25:51 +0200

    Zadania JS

--- 05_Kontrola_przeplywu_-_Petle/1_Tworzenie tablicy - for/js/app.js
+++ 05_Kontrola_przeplywu_-_Petle/1_Tworzenie tablicy - for/js/app.js
@@ -1,4 +1,10 @@
 const array = [];
 const n = 12;
 // Piszcie kod pod tym komentarzem.
+for (i = 0; i <= n; i++) {
+    array.push(i);
+}
+console.log(array);
+
+
 
--- 05_Kontrola_przeplywu_-_Petle/2_Liczby parzyste i nieparzyste - for/js/app.js
+++ 05_Kontrola_przeplywu_-_Petle/2_Liczby parzyste i nieparzyste - for/js/app.js
@@ -1,3 +1,13 @@
-const n = 13;
+
+    const n = 13;
 // Piszcie kod pod tym komentarzem.
+for (i = 0; i <= n; i++) {
+    if(i % 2 == 0) {
+        console.log(i + " parzyste");
+    }
+    else {
+        console.log(i + " nieparzyste");
+    }
+}
+
 
