391fbdf922d20f036f3859cc2f19fc68a711bd9f
wojtek0126 <wojtek0126@gmail.com>
2020-06-10 19:29:32 +0200

    Zadania JS

--- 05_Kontrola_przeplywu_-_Petle/7_Tworzenie tablicy 2D/js/app.js
+++ 05_Kontrola_przeplywu_-_Petle/7_Tworzenie tablicy 2D/js/app.js
@@ -1,5 +1,19 @@
-const array = [];
+// Napiszcie program, który w zmiennej array utworzy
+// tablicę wielowymiarową zgodnie z wartościami w zmiennych rows i cols.
+// Elementami tablicy mają być kolejne liczby całkowite zaczynając od jedynki.
+
+let array = [];
 const cols = 4;
 const rows = 5;
-// Piszcie kod pod tym komentarzem.
+let counter = 1;
+for(i = 0;i < cols; i++) {
+    array.push([]);
+    for(j =0; j <= rows -1; j++){
+        array[i].push(counter);
+        counter++;
+    }
+}
+
+console.log(array);
+console.log((array[1].length));
 
--- 05_Kontrola_przeplywu_-_Petle/8_Tabliczka mnożenia/js/app.js
+++ 05_Kontrola_przeplywu_-_Petle/8_Tabliczka mnożenia/js/app.js
@@ -1,5 +1,24 @@
 const n = 3;
 const calc = [];
 // Piszcie kod pod tym komentarzem.
+// Tabliczka mnożenia
+// Napiszcie program tworzący tabliczkę mnożenia dla podanej zmiennej n.
+//     Wyniki zwróćcie w tablicy dwu wymiarowej (w zmiennej calc). Wartości w tej tablicy mają być tekstami obliczeń.
+//     Na przykład dla n = 3 wynik będzie następujący:
+
+//n to bedzie liczba, i to mnoznik, zrobie petle
+for (i = 1; i <= n; i++){
+    calc.push(n * i);
+
+
+    // for (j = 0; j <= calc.length; j++){
+    //
+    // }
+
+}
+
+
+
+
 
 
