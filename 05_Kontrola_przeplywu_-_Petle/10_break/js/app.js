0d1169d0d03bea5cc9c0ef089480d7895d4d5e31
wojtek0126 <wojtek0126@gmail.com>
2020-06-11 22:41:38 +0200

    Zadania JS

--- 05_Kontrola_przeplywu_-_Petle/10_break/js/app.js
+++ 05_Kontrola_przeplywu_-_Petle/10_break/js/app.js
@@ -1,6 +1,9 @@
 let i = 0;
 while (true) {
   // Piszcie kod pod tym komentarzem.
-
-
+    console.log("...loading" + " level: " + i);
+    i++
+    // if (i = 5){
+    //     break
+    // }
 }
--- 05_Kontrola_przeplywu_-_Petle/9_Gwiazdki/js/app.js
+++ 05_Kontrola_przeplywu_-_Petle/9_Gwiazdki/js/app.js
@@ -2,7 +2,7 @@
 const n = 5;
 let symbol = "*";
 // Piszcie kod pod tym komentarzem.
-for (i = 0; i < 6;i++) {
+for (i = 0; i < n;i++) {
     console.log(symbol);
     symbol += "*";
 }
