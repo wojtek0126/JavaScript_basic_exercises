3db9fb20d8785bda329caf3ab7f976057cf3f91d
wojtek0126 <wojtek0126@gmail.com>
2020-06-11 22:27:48 +0200

    Zadania JS

--- 05_Kontrola_przeplywu_-_Petle/8_Tabliczka mnożenia/js/app.js
+++ 05_Kontrola_przeplywu_-_Petle/8_Tabliczka mnożenia/js/app.js
@@ -1,42 +1,21 @@
 
-// Piszcie kod pod tym komentarzem.
-// Tabliczka mnożenia
-// Napiszcie program tworzący tabliczkę mnożenia dla podanej zmiennej n.
-//     Wyniki zwróćcie w tablicy dwu wymiarowej (w zmiennej calc). Wartości w tej tablicy mają być tekstami obliczeń.
-//     Na przykład dla n = 3 wynik będzie następujący:
-
-//n to bedzie liczba, i to mnoznik, zrobie petle
-//
 const n = 3;
 const calc = [];
-let m = 1;
-for (i = 0; i<= n - 1;i++) {
-        calc.push([]);
-        let j = 1;
-        while(j != n + 1){
-            calc[i].push(m + " * "+ (n - (n -1)) + " =  " + m * (n - (n -1)));
-            j++;
-            m++;
-//wip
-        // for (j = 0; j <= n - 1; j++) {
-        //     // let result = toString(n * m);
-        //     calc[i].push(n * m);
-        //     m++;
-        }
-        // let temp = (n * i).toString();
 
+for (i = 0; i < n; i++) {
+    calc.push([]);   //to n razy zrobi pusty zbior w zbiorze calc
+    for (j = 0; j < n; j++) { // to w kazdy zbior z petli powyzej wpakuje to co jest ponizej n razy
+     let result = ((i + 1) + " * " + (j + 1) + " = " + (i + 1) * (j + 1)); //wynik i + 1 i j + 1 zeby nie zaczynalo od 0
+     let stringedRes = result.toString() //wynik przerabiam na stringa
+     calc[i].push(stringedRes); //  przestringowany wynik wrzucam do podzbioru
+    }
 }
-console.log(calc);
-    // digit = n.toString();
-    // multiplier = i.toString();
-    // result = calc[x].toString();
-    // console.log(calc[x]);
-    // console.log(digit + " * " + multiplier + " = " + result);
-// console.log(calc)
-// }
-// for (j = 0; j <= n; j++){
-//     console.log(calc[j]);
-// }
+for (i = 0; i < n;i++) {
+    console.log(calc[i]);
+}
+
+console.log(calc.);
+console.table(calc);  //wyswietli wynik w konsoli w formie tabeli
 
 
 
