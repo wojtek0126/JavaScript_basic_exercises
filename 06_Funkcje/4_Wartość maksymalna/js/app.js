df9b0ef2378bcd61d1ea9fdee66f7fb7b9bd8609
wojtek0126 <wojtek0126@gmail.com>
2020-06-13 00:10:38 +0200

    Zadania JS

--- 06_Funkcje/4_Wartość maksymalna/js/app.js
+++ 06_Funkcje/4_Wartość maksymalna/js/app.js
@@ -1,2 +1,19 @@
+// wersja pierwsza
 const randomNumbers = [27, 64, 47, 78, 48, 11, 76, 25, 11, 83];
+function maxFromArray(numbers) {
+   let top = 0;
+    for (i = 0; i < numbers.length;i++){
+        if (numbers[i] > top) {
+            top = numbers[i];
+        }
+    }
+    return top;
+}
+// wersja druga,
+const temp = [333, 666, 999, 3333];
+function maxFromArrayShort(numbers2) {
+    return Math.max(...numbers2);   //bez tych trzech kropek Math.max nie zadziala na zbiorze
+}
 
+console.log(maxFromArray(randomNumbers));
+console.log(maxFromArrayShort(temp));
