18807a03e93c461c36abf4c5eb5d2ac3ff5bc44d
wojtek0126 <wojtek0126@gmail.com>
2020-06-12 23:46:34 +0200

    Zadania JS

--- 06_Funkcje/3_Wyświetlanie tablicy 2D/js/app.js
+++ 06_Funkcje/3_Wyświetlanie tablicy 2D/js/app.js
@@ -1,2 +1,15 @@
 
+const users = [
+  ["Jan", "Nowak"],
+  ["Zosia","Kowalska"]
+];
 
+function printArray2D(array2D) {
+    for(i = 0; i < array2D.length;i++){
+        for(j = 0; j < array2D[i].length;j++){
+            console.log(array2D[i][j]);
+        }
+    }
+}
+
+printArray2D(users);
