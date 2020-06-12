5d7996dfabe68441a00fb4231c8750bdc1e280bc
wojtek0126 <wojtek0126@gmail.com>
2020-06-12 23:14:12 +0200

    Zadania JS

--- 05_Kontrola_przeplywu_-_Petle/8_Tabliczka mnożenia/js/app.js
+++ 05_Kontrola_przeplywu_-_Petle/8_Tabliczka mnożenia/js/app.js
@@ -4,18 +4,17 @@ const calc = [];
 
 for (i = 0; i < n; i++) {
     calc.push([]);   //to n razy zrobi pusty zbior w zbiorze calc
-    for (j = 0; j < n; j++) { // to w kazdy zbior z petli powyzej wpakuje to co jest ponizej n razy
+    for (j = 0; j < 3; j++) { // to w kazdy zbior z petli powyzej wpakuje to co jest ponizej n razy
      let result = ((i + 1) + " * " + (j + 1) + " = " + (i + 1) * (j + 1)); //wynik i + 1 i j + 1 zeby nie zaczynalo od 0
      let stringedRes = result.toString() //wynik przerabiam na stringa
      calc[i].push(stringedRes); //  przestringowany wynik wrzucam do podzbioru
-     let x = calc[i][j];
     }
+}
+for(i =0;i < n;i++){
+    console.log(calc[i][0], " | ", calc[i][1], " | ", calc[i][2]); // druga petla zeby to wyswietlic
 }
 
 
-console.log(calc);
-console.log("Typ każdego elementu w powyższych tablicach to: " + typeof calc[1][1]);
-// console.table(calc);  //wyswietli wynik w konsoli w formie tabeli
 
 
 
--- 06_Funkcje/1_Tworzenie tablicy/js/app.js
+++ 06_Funkcje/1_Tworzenie tablicy/js/app.js
@@ -1,2 +1,18 @@
 
+function createArray(rows) {
+    array = [];
+    let counter = 1;
+    for (i = 0; i < rows; i++) {
+        array.push([]);
+        for (j = 0; j < rows;j++) {
+            array[i].push(counter);
+            counter++;
+        }
 
+
+        return array;
+    }
+}
+
+createArray(5);
+console.log(array); //WIP
