3481512b8ff7f04479bd47c499b0678ad7e06d41
wojtek0126 <wojtek0126@gmail.com>
2020-06-12 23:39:15 +0200

    Zadania JS

--- 06_Funkcje/2_Wyświetlanie tablicy/js/app.js
+++ 06_Funkcje/2_Wyświetlanie tablicy/js/app.js
@@ -1,2 +1,12 @@
 
+const people = ["John", "Ive", "Donna", "Chris"];
+
+function printArray(array) {
+    for (i =0 ;i < array.length;i++){
+        console.log(array[i]);
+    }
+
+}
+printArray(people);
+
 
