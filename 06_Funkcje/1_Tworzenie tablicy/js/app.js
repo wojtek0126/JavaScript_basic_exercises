2d4aa247fcda8fa2758a71dda178b16a73118829
wojtek0126 <wojtek0126@gmail.com>
2020-06-12 23:32:56 +0200

    Zadania JS

--- 06_Funkcje/1_Tworzenie tablicy/js/app.js
+++ 06_Funkcje/1_Tworzenie tablicy/js/app.js
@@ -1,18 +1,11 @@
 
 function createArray(rows) {
     array = [];
-    let counter = 1;
-    for (i = 0; i < rows; i++) {
-        array.push([]);
-        for (j = 0; j < rows;j++) {
-            array[i].push(counter);
-            counter++;
-        }
-
-
-        return array;
+    for (i = 1; i <= rows; i++) {
+            array.push(i);
     }
+    return array;
 }
 
 createArray(5);
-console.log(array); //WIP
+console.log(array);
